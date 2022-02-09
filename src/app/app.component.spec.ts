a9383708f4d098f9eb3609d1fb87172000161a8d
Anthony <anthonymarques@outlook.com>
2022-02-09 19:44:50 +0200

    setup jest for angular

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,11 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('gk-weather-app');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('gk-weather-app app is running!');
-  });
 });
