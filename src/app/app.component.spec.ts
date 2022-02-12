028dc89ab2c607566e1412b0d93735713d713e1d
Anthony <anthonymarques@outlook.com>
2022-02-13 01:34:01 +0200

    small setup for unit test fixed unit test issue sort of

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,22 +1,51 @@
-import { TestBed, async } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { CelcuisConverter } from './celcuis-converter.directive';
+import { Mocks } from './mocks/mock-data';
+import { MockApiService } from './mocks/mock-services';
+import { WeatherService } from './weather.service';
+
+
+const mockSuccess = (success, error) => {
+  success({ coords: Mocks.position.coords });
+  error({ error: 'error message' });
+}
+
+const mockGetcurrentPosition = jest.fn(mockSuccess);
+const mockSuccessHandler = jest.fn(() => Mocks.position.coords);
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let navigatorMock;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent, CelcuisConverter
       ],
+      imports: [BrowserModule, HttpClientModule],
+      providers:[
+        {provide: WeatherService, useClass: MockApiService},
+        {provide: global.navigator.geolocation, useValue: mockGetcurrentPosition},
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
+    // global.navigator.geolocation = { getCurrentPosition: mockGetcurrentPosition }
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    component.ngOnInit();
+    fixture.detectChanges();
   }));
-
+  
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'gk-weather-app'`, () => {
+        navigatorMock = jest.spyOn(global.navigator.geolocation, 'getCurrentPosition').mockImplementation();
+        expect(component).toBeTruthy();
+      });
+      
+      it(`should have as title 'gk-weather-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('gk-weather-app');
