893d4bc1adb9da33849071a468b98d3954be36fb
Anthony <anthonymarques@outlook.com>
2022-02-14 22:33:45 +0200

    code clean up

--- src/app/components/loader/loader.component.ts
+++ src/app/components/loader/loader.component.ts
@@ -1,15 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loader',
   templateUrl: './loader.component.html',
   styleUrls: ['./loader.component.css']
 })
-export class LoaderComponent implements OnInit {
-
+export class LoaderComponent {
   @Input() messagePrompt:string = 'Loading please wait'
-
-  ngOnInit(): void {
-  }
 
 }
